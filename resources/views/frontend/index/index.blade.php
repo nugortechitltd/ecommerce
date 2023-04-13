f789997864f977696726a7541a94d4367c9a3058
nugortechitltd <nugortechit@gmail.com>
2023-04-13 16:20:51 +0600

    thnakssjhkjhbkjdf

--- resources/views/frontend/index/index.blade.php
+++ resources/views/frontend/index/index.blade.php
@@ -271,7 +271,9 @@
                             </a>
                             <div class="bottom-content">
                                 <div class="star-rating">
-                                    {{-- @php
+                                    @php
+                                    $total_review = Orderproduct::where('product_id', $feat_product->id)->where('review', '!=', null)->count();
+                                    $total_star = OrderProduct::where('product_id', $feat_product->id)->where('review', '!=', null)->sum('star');
                                     $total_rating = 0;
                                     if($total_review != 0) {
                                         $total_rating = $total_star / $total_review;
@@ -285,7 +287,7 @@
                                     for ($j = $total_rating + 1 ; $j <= 5; $j++) {
                                         echo '<i class="far fa-star"></i>';
                                     }
-                                    @endphp --}}
+                                    @endphp
                                     
                                 </div>
                                 <a href="{{route('product.details', $feat_product->slug)}}" class="product-name">{{$feat_product->product_name}}</a>
